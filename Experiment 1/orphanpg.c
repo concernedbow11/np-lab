7cdb5f0facfbb9bd326435299b4d2aa63fdae310
Sahil Lukose <44937743+concernedbow11@users.noreply.github.com>
2021-03-29 01:21:38 +0530

    Create orphanpg.c

---
+++ Experiment 1/orphanpg.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+int main(){
+   pid_t child_pid = fork();
+   // Parent process
+   if (child_pid > 0){
+       printf("parent\n");
+   }
+   // Child process
+   else if (child_pid==0){       
+       sleep(15);
+       printf("child process");
